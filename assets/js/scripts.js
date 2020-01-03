05eb0a8d6dc79019b15d38bf416058c868eccacf
Samuele de Tomasi <el3um4s@gmail.com>
2020-01-03 17:53:39 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -116,7 +116,7 @@ Version      : 1.0
                 }
             });
         };
-        // Call the functions 
+        // Call the functions
         magnifPopup();
 
 
@@ -178,13 +178,13 @@ Version      : 1.0
         });
 
 
-        
+
         /*
          * ----------------------------------------------------------------------------------------
          *  AJAX JS
          * ----------------------------------------------------------------------------------------
          */
-	
+
 	// Function for email address validation
 	function isValidEmail(emailAddress) {
         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
@@ -192,33 +192,33 @@ Version      : 1.0
 		return pattern.test(emailAddress);
 
 	}
-	$("#contactForm").on('submit', function (e) {
-		e.preventDefault();
-		var data = {
-			name: $("#name").val(),
-			email: $("#email").val(),
-            subject: $("#subject").val(),
-			message: $("#message").val()
-		};
+	// $("#contactForm").on('submit', function (e) {
+	// 	e.preventDefault();
+	// 	var data = {
+	// 		name: $("#name").val(),
+	// 		email: $("#email").val(),
+  //           subject: $("#subject").val(),
+	// 		message: $("#message").val()
+	// 	};
+  //
+	// 	if (isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1)) {
+	// 		$.ajax({
+	// 			type: "POST",
+	// 			url: "sendmail.php",
+	// 			data: data,
+	// 			success: function () {
+	// 				$('#contactForm .input-success').delay(500).fadeIn(1000);
+	// 				$('#contactForm .input-error').fadeOut(500);
+	// 			}
+	// 		});
+	// 	} else {
+	// 		$('#contactForm .input-error').delay(500).fadeIn(1000);
+	// 		$('#contactForm .input-success').fadeOut(500);
+	// 	}
+  //
+	// 	return false;
+	// });
 
-		if (isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1)) {
-			$.ajax({
-				type: "POST",
-				url: "sendmail.php",
-				data: data,
-				success: function () {
-					$('#contactForm .input-success').delay(500).fadeIn(1000);
-					$('#contactForm .input-error').fadeOut(500);
-				}
-			});
-		} else {
-			$('#contactForm .input-error').delay(500).fadeIn(1000);
-			$('#contactForm .input-success').fadeOut(500);
-		}
-
-		return false;
-	});
-        
 
         /*
          * ----------------------------------------------------------------------------------------
